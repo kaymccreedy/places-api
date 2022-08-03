6981db6dfab91695af1e72cade76c010fe5cca80
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-08-03 11:54:24 -0400

    creates Rails backend for places app, defines controller actions, creates Place model, sets validations

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Place.create(name: "The White House", address: "1600 Pennsylvania Avenue, Washington DC")
+Place.create(name: "New York Stock Exchange", address: "11 Wall Street New York, NY")
+Place.create(name: "The Empire State Building", address: "350 Fifth Avenue New York, NY 10118")
+Place.create(name: "The Sherlock Holmes Museum", address: "221 B Baker St, London, England")
+Place.create(name: "The Eiffel Tower", address: "Tour Eiffel Champ de Mars, Paris")
